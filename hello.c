3b9fdffe9115b2ff957071f7584e8a4ff840a0f0
Farid-N <fnaif5@huskers.unl.edu>
2024-09-03 21:14:33 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+    printf("Hello, I am Farid Naif/n");
+    printf("Major is computer science/n");
+    return 0;
+ }
