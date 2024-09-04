90785be8aa8a6111a4e7a31b96095815d135936a
Farid-N <fnaif5@huskers.unl.edu>
2024-09-04 12:44:57 -0500

    Test

--- hello.c
+++ hello.c
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
 
     printf("Hello, I am Farid Naif/n");
     printf("Major is computer science/n");
-    printf("Minor is undeclared!!!!!!!!!!!\n");
+    printf("Minor is undeclared!!!123455!\n");
 
     return 0;
  }
