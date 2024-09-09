bcc793ba475a374c4bfa9555bb8e5e062fd609e0
Farid-N <fnaif5@huskers.unl.edu>
2024-09-09 13:31:18 +0000

    Mon, Sep 9, 2024, 8:31 AM -05:00

--- airDistance.c
+++ airDistance.c
@@ -14,42 +14,39 @@
     #define PI 3.14159265358979323846
 
     double degrees_to_radians(double degree) {
-
-        return degree * PI / 180;
+    return degree * PI / 180;
     }
 
     int main () {
-        double lat1, lon1, lat2, lon2;
-        double d, delta_lon;
+    double lat1, lon1, lat2, lon2;
+    double d, delta_lon;
 
-        printf("Enter the latitude of the origin (in degrees): ");
-        scanf("%lf", &lat1);
+    printf("Enter the latitude of the origin (in degrees): ");
+    scanf("%lf", &lat1);
 
-        printf("Enter the longitude of the origin (in dgrees): ");
-        scanf("%lf", &lon1);
+    printf("Enter the longitude of the origin (in degrees): ");
+    scanf("%lf", &lon1);
 
-        printf("Enter the latitude of the destination (in dgrees): ");
-        scanf("%lf", &lat2);
+    printf("Enter the latitude of the destination (in degrees): ");
+    scanf("%lf", &lat2);
 
-        printf("Enter the longitude of the  (in dgrees): ");
-        scanf("%lf", &lon2);
+    printf("Enter the longitude of the destination (in degrees): ");
+    scanf("%lf", &lon2);
 
-        lat1 = degrees_to_radian(lat1);
-        lon1 = degrees_to_radians(lot1);
-        lat2 = degrees_to_radians(lat2);
-        lon2 = degrees_to_radians(lon2);
+    lat1 = degrees_to_radians(lat1);
+    lon1 = degrees_to_radians(lon1);
+    lat2 = degrees_to_radians(lat2);
+    lon2 = degrees_to_radians(lon2);
 
-        delta_lon = lon2 - lon1;
+    delta_lon = lon2 - lon1;
 
-        d = acos(sin(lat1) * sin(lat2) + cos(lat2) * cos(lat2) * cos(delta_lon)) * R;
+    d = acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(delta_lon)) * R;
 
-        printf("\nLocatoin Distance\n");
-        printf("========");
-        printf("Origin: (%lf, &lf)\n", lat1 * 180 / PI, lon1 * 180 / PI);
-        printf("Destination: (&lf, &lf)\n", lat2 * 180 / PI, lon2 * 180 / PI);
-        printf("Air distance is %lf km\n", d);
+    printf("\nLocation Distance\n");
+    printf("========\n");
+    printf("Origin: (%lf, %lf)\n", lat1 * 180 / PI, lon1 * 180 / PI);
+    printf("Destination: (%lf, %lf)\n", lat2 * 180 / PI, lon2 * 180 / PI);
+    printf("Air distance is %lf km\n", d);
 
-
-
-        return 0;
-    }
+    return 0;
+}
