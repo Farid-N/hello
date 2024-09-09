3c428104cc3ed9d6a406cacb2f6b523a9df9371e
Farid-N <fnaif5@huskers.unl.edu>
2024-09-09 13:16:47 +0000

    Mon, Sep 9, 2024, 8:16 AM -05:00

--- airDistance.c
+++ airDistance.c
@@ -10,4 +10,48 @@
     #inlcude <stdio.h>
     #inlcude <math.h>
 
-    
+    #define R 6371
+    #define PI 3.14159265358979323846
+
+    double degrees_to_radians(double degree) {
+
+        return degrees * PII / 180.0
+    }
+
+    int main () {
+        double la1, lon1, lat2, lon2;
+        double d, delta_lon:
+
+        printf("Enter the latitude of the origin (in degrees): ");
+        scanf("%lf", &lat1);
+
+        printf("Enter the longitude of the origin (in dgrees): ");
+        scanf("%lf", &lon1);
+
+        printf("Enter the latitude of the destination (in dgrees): ");
+        scanf("%lf", &lat2);
+
+        printf("Enter the longitude of the  (in dgrees): ");
+        scanf("%lf", &lon2);
+
+        lat1 = degrees_to_radian(lat1);
+        lon1 = degrees_to_radians(lot1);
+        lat2 = degrees_to_radians(lat2):
+        lon2 = degrees_to_radians(lon2);
+
+        delta_lon = lon2 - lon1:
+
+        d = acos(sin(lat1) * sin(lat2) + cos(lat2) * cos(lat2) * cos(delta_lon)) * R;
+
+        printf("\nLocatoin Distance\n");
+        printf("========");
+        printf("Origin")
+
+
+
+
+
+
+
+        return 0;
+    }
