5e2f78275f6181ce680be8a739980585f135b00e
Farid-N <fnaif5@huskers.unl.edu>
2024-09-09 13:02:01 +0000

    Mon, Sep 9, 2024, 8:02 AM -05:00

--- airDistance.c
+++ airDistance.c
@@ -2,4 +2,12 @@
      * Author: Farid Naif
      * Date: Semptember 9, 2024
      *
-     * This program 
+     * This program is about calculating the air distance between two locations
+     * on Earth. Given their latitude and longitude in degrees.
+     */
+
+
+    #inlcude <stdio.h>
+    #inlcude <math.h>
+
+    
